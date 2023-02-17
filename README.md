644a969293676097234e438bff2cfd318d10fbf2
Sidharth Raja <sidharth15.raja@gmail.com>
2023-02-17 00:44:09 -0800

    Add post template + basic post page generation.

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Feo
 A no-frills static site generator written in Rust.
 
-### Regenerate everything
+### Writing
+
+Write all your posts in Markdown (i.e. with an .md extension) in `/posts`.  Make sure to start each post with the YAML metadata to help the generator. 
+
+```
+---
+slug: getting-rusty
+title: Getting Rusty
+published: true
+publish_date: 2023-02-15
+---
+
+Long years ago, we made a tryst with destiny. Now the time comes, we must redeem our pledge...
+```
+
+### Building
+
+Run this from the root directory of the project.
+
 ```
 cargo run
 ```
@@ -13,7 +31,11 @@ Ingestion:
 [✅] Parse all content into Post struct types.  
 
 Generation:  
-[ ] Define post template.  
+[✅] Define post template.  
+[✅] Generate posts from post array.  
 [ ] Define index template.  
-[ ] Generate posts from post array.  
 [ ] Generate index from post array.  
+
+Styling:
+[] Make default posts page look good.
+[] Make default index page look good.
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,43 @@ extern crate yaml_rust;
 
 mod models;
 
+use handlebars::Handlebars;
+use std::collections::BTreeMap;
 
 fn main() {
-    let posts = get_posts();
-    // print number of posts.
-    println!("Parsed {} post(s).", posts.len());
+    let posts = parse_posts();
+
+    for post in &posts {
+        let generated_html = get_post_page(&post);
+        println!("{}", generated_html);
+        // Create directory if it doesn't exist.
+        std::fs::create_dir_all("gen").unwrap();
+        // Save the generated HTML to a file.
+        std::fs::write(format!("gen/{}.html", post.meta.slug), &generated_html).unwrap();
+    }
+    println!("Generated {} post(s).", posts.len());
+    
+}
+
+fn get_post_page(post: &models::Post) -> String {
+    // Get the post template from the file.
+    let mut handlebars = Handlebars::new();
+    let post_template = std::fs::read_to_string("templates/post.html").unwrap();
+    // Register the template, and check that it's okay.
+    assert!(handlebars.register_template_string("post_template", post_template).is_ok());
+
+    // Put the post data into a 1:1 template map.
+    let mut data = BTreeMap::new();
+    data.insert("title", &post.meta.title);
+    data.insert("publish_date", &post.meta.publish_date);
+    data.insert("content", &post.html);
+
+    // Render the data onto the template.
+    return handlebars.render("post_template", &data).unwrap();
 }
 
 // Read all markdown files in the posts directory.
-fn get_posts() -> Vec<models::Post> {
+fn parse_posts() -> Vec<models::Post> {
     let mut posts = Vec::new();
 
     for entry in std::fs::read_dir("posts").unwrap() {
